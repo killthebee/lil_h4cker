6906287256a611542c4d33eca5fe7a73537cb2dc
killthebee <51859323+killthebee@users.noreply.github.com>
2020-01-31 17:32:56 +0200

    fix commendation script, move imports

--- scripts.py
+++ scripts.py
@@ -1,20 +1,19 @@
+from datacenter.models import Schoolkid, Lesson, Subject, Commendation, Chastisement
+import random as r
+
+
 def fix_marks(schoolkid_name):
-    from datacenter.models import Schoolkid, Mark
     schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
     Mark.objects.filter(schoolkid=schoolkid, points__lt=4).update(points=5)
 
 
 def remove_chastisements(schoolkid_name):
-    from datacenter.models import Schoolkid, Chastisement
     schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
     chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
     chastisements.delete()
 
 
 def create_commendation(schoolkid_name, subject):
-    from datacenter.models import Schoolkid, Lesson, Subject, Commendation
-
-    import random as r
     commendations = [
         'Молодец!',
         'Отлично!',
@@ -24,9 +23,12 @@ def create_commendation(schoolkid_name, subject):
     ]
 
     schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-    targeted_subject = Subject.objects.get(title=subject, year_of_study=schoolkid.year_of_study)
-    lessons = Lesson.objects.filter(group_letter=schoolkid.group_letter, subject=targeted_subject)
-    last_lesson = lessons.last()
+    lessons = Lesson.objects.filter(
+        group_letter=schoolkid.group_letter,
+        subject__title=subject,
+        year_of_study=schoolkid.year_of_study
+    )
+    last_lesson = lessons.order_by('date').last()
     Commendation.objects.create(
         text=r.choice(commendations),
         created=last_lesson.date,
